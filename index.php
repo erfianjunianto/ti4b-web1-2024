201717dfb1edb9cbd2028ce338c2bab6c9ec02f8
Notiva <notiva@Notivas-MacBook-Pro.local>
2024-03-28 15:11:54 +0700

    init

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>TI 4A</title>
+</head>
+<body>
+    <h1>Pemrograman WEB 1</h1>
+    <p>Program Studi Teknik Informatika</p>
+</body>
+</html>
