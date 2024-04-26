6d8618c1d8addd1be2070e6095715bf8a0371c02
Notiva <notiva@Notivas-MacBook-Pro.local>
2024-04-26 20:06:55 +0700

    update materi pertemuan 4

--- index.php
+++ index.php
@@ -1,13 +1,33 @@
-<!DOCTYPE html>
+<!DOCTYPE html> <!-- dokumen ini adalah format html5 -->
 <html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>TI 4A</title>
-</head>
-<body>
-    <h1>Pemrograman WEB 1</h1>
-    <p>Program Studi Teknik Informatika</p>
-</body>
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>WEB 1 TI.4B</title>
+    </head>
+    <body>
+        Hyperlink => <a href="/form/register.php">Register Akun</a> <!-- anchor -->
+        <h1>Ini Dokumen Index.php</h1>
+        <p>Tag < p > merupakan tag untuk paragrap </p>
+        <ol type="a">
+            <li>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s</li>
+            <li>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s 2</li>
+            <li>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s 3</li>
+            <li>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s 4</li>
+            <li>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s 5</li>
+            <li>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s 6</li>
+        </ol>
+
+        <ul type="square">
+            <li>Lorem Ipsum is simply <br> dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s 1</li>
+            <li>Lorem Ipsum is simply dummy <br> text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s 2</li>
+            <li>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s 3</li>
+            <li>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s 4</li>
+            <li>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s 5</li>
+            <li>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s 6</li>
+        </ul>
+
+        <hr>
+    </body>
 </html>
