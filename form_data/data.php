8362a55a17957666989f332be83645fe1f3b64e0
Notiva <notiva@Notivas-MacBook-Pro.local>
2024-05-03 19:52:32 +0700

    update materi pertemuan 5

---
+++ form_data/data.php
@@ -0,0 +1,8 @@
+<?php
+echo "<pre>";
+    echo "Nama Lengkap: ".$_POST['nama_lengkap']."<br>";
+    echo "Jenis Kelamin: ".$_POST['j_kelamin']."<br>";
+    echo "Kota: ".$_POST['kota']."<br>";
+    echo "Alamat: ".$_POST['alamat']."<br>";
+echo "</pre>";
+?>
